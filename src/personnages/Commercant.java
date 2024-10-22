08dc9452e929458a092050cf29140ecaee41d12d
Nolawi Gebrekirstos <gbn5079a@ensfsi.onmicrosoft.com>
2024-10-22 19:57:54 +0200

    création de la classe Commercant

---
+++ src/personnages/Commercant.java
@@ -0,0 +1,20 @@
+package personnages;
+
+public class Commercant extends Humain {	
+	public Commercant(String nom, int argent) {
+		super(nom, "thé", argent); 
+		}
+	
+	public int seFaireExtorquer() {
+		int argent = getArgent();
+		perdreArgent(getArgent());
+		parler("J'ai tous perdu! Le monde est trop injuste... ");
+		return argent;
+	}
+	
+	public void recevoir(int argent) {
+		parler(argent + " sous! Je te remercie généreux donateur!");
+		gagnerArgent(argent);
+		}
+	
+}
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -39,11 +39,11 @@ public class Humain {
 		
 	}
 	
-	private void gagnerArgent(int gain) {
+	public void gagnerArgent(int gain) {
 		argent +=gain;
 	}
 	
-	private void perdreArgent(int perte) {
+	public void perdreArgent(int perte) {
 		argent += perte;
 	}	
 	
