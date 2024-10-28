b97f148cf10a05ccd18810827544109e56052200
Nolawi Tewodros <nolex2005@gmail.com>
2024-10-28 14:20:52 +0100

    creation de la classe Ronin

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -2,15 +2,15 @@ package histoire;
 
 import personnages.Commercant;
 import personnages.Ronin;
+import personnages.Yakuza;
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
 		Commercant marco = new Commercant("Marco", 15);
-		Ronin yaku = new Ronin("Yaku Le Noir", "whisky", 30, "Warsong");
-		yaku.direBonjour();
-		yaku.parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
-		yaku.parler(marco.getNom()+ ", si tu tiens à la vie donne moi ta bourse !");
-		yaku.extorquer(marco);
+		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+		Ronin roro = new Ronin("Roro", "shochu", 60);
+		roro.direBonjour();
+		roro.donner(marco);
 
 	}
 }
--- src/personnages/Ronin.java
+++ src/personnages/Ronin.java
@@ -1,24 +1,16 @@
 package personnages;
 
-public class Ronin extends Humain {
-	private String clan;
-	private int reputation = 0;
-	public Ronin(String nom, String boissonFavorite, int argent, String clan) {
-		super(nom, boissonFavorite, argent);
-		this.clan = clan;
-		}
-	
-	public void extorquer(Commercant victime) {
-		int argentVictime = victime.getArgent();
-		victime.seFaireExtorquer();
-		parler("J’ai piqué les " + argentVictime + " sous de " + victime.getNom() + ", ce qui me fait " + gagnerArgent(argentVictime) + " sous dans ma poche. Hi ! Hi !");
+public class Ronin extends Humain{
+	private int honneur = 1;
 
+	public Ronin(String nom, String boissonFavorite, int argent) {
+		super(nom, boissonFavorite, argent);
 	}
 	
-	
-	
-	
-	
-	
+	public void donner(Commercant beneficiaire) {
+		int argentDonner = getArgent() / 10;
+		parler("Marco prend ces " + argentDonner + " sous.");
+		beneficiaire.recevoir(argentDonner);
+	}
 
 }
